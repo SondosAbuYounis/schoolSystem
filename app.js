f0fe9987fb64f0bceee7eb4779d51ec56bae9f5b
SondosAbuYounis <abuyounissundos@gmail.com>
2023-10-07 03:22:38 +0300

    styling the page is done

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-// You have to read all the data that the user will fill the form with after the submission 
-// display it in a new row (so you have to create a row for each student using dom manipulation ) 
-// then append it to the table that we have in the html 
+// You have to read all the data that the user will fill the form with after the submission
+// display it in a new row (so you have to create a row for each student using dom manipulation )
+// then append it to the table that we have in the html
+
+// append child tr
+
+constructor
+// You will create a constructor to generate a student object which will be rendered in the main section
+// You will refactor the render function to render each student information in a separate card in the home page
+// Note that you will use images for the students in the assets directory.
+// Add all the students in an array and save it to the local storage.
